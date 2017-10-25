3947abf5afead8f96e1b1f8350c8ed6166a8a26c
Anderson, Daniel <daniel.anderson28562@hotmail.com>
2017-10-25 16:22:16 -0400

    Add the Category and Pie Models with appropriate interfaces, repositories, and Dependency Injection

---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+
+namespace BethanysPieShop.Models
+{
+    interface IPieRepository
+    {
+        IEnumerable<Pie> Pies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        Pie GetPieById(int pieId);
+    }
+}
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +13,9 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
             services.AddMvc();
         }
 
