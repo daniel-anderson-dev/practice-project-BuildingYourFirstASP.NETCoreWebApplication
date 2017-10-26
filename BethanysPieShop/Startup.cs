aee84ec44f7b5335955cd5825cfc88ef51d48f89
Anderson, Daniel <daniel.anderson28562@hotmail.com>
2017-10-26 12:59:22 -0400

    Create the Shopping Cart and all of its related items (View, Models, apparently no Interfaces, etc).

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -30,8 +30,13 @@ namespace BethanysPieShop
 
             services.AddTransient<ICategoryRepository, CategoryRepository>();
             services.AddTransient<IPieRepository, PieRepository>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
 
             services.AddMvc();
+
+            services.AddMemoryCache();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +49,7 @@ namespace BethanysPieShop
 
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseMvcWithDefaultRoute();
 
             DbInitializer.Seed(app);
