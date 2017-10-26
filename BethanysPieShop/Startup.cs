ae20c4b87a93bac7f1612d7d324ee37c40b9861b
Anderson, Daniel <daniel.anderson28562@hotmail.com>
2017-10-26 10:12:22 -0400

    Setup the basic Entity Framework configuration

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -9,10 +11,22 @@ namespace BethanysPieShop
 {
     public class Startup
     {
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
             services.AddTransient<IPieRepository, MockPieRepository>();
 
