ac7e3be09a87d1613d055db7feb0c2e5027b50b5
Daniel Anderson <daniel.anderson28562@hotmail.com>
2017-11-07 10:31:03 -0500

    Add a basic API endpoint and retrieve data dynamically via jQuery

---
+++ BethanysPieShop/Controllers/PieDataController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BethanysPieShop.Models;
+using System.Collections.Generic;
+using BethanysPieShop.ViewModels;
+using System.Linq;
+
+namespace BethanysPieShop.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class PieDataController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public PieDataController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<PieViewModel> LoadMorePies()
+        {
+            var dbPies = _pieRepository.Pies.OrderBy(p => p.PieId).Take(10);
+
+            List<PieViewModel> pies = new List<PieViewModel>();
+
+            foreach (var dbPie in dbPies)
+            {
+                pies.Add(MapDbPieToPieViewModel(dbPie));
+            }
+            return pies;
+        }
+
+        private PieViewModel MapDbPieToPieViewModel(Pie dbPie)
+        {
+            return new PieViewModel()
+            {
+                PieId = dbPie.PieId,
+                Name = dbPie.Name,
+                Price = dbPie.Price,
+                ShortDescription = dbPie.ShortDescription,
+                ImageThumbnailUrl = dbPie.ImageThumbnailUrl
+            };
+        }
+
+    }
+}
